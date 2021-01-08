805112d32c57736582cc663cda6f2ccc50a14805
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-08 13:14:11 +0530

    Added:function for first name validation

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const print = require('./utility');
 const utility=require('./utility')
 console.log("Welcome to user-registration")
-
+utility.firstNameValidator();
--- utility.js
+++ utility.js
@@ -1,5 +1,14 @@
-
+const readline = require('readline-sync')
+let NAME_PATTERN = new RegExp("^[A-Z]{1}[a-z]{2,}$");
 class Utility {
- 
+   firstNameValidator=(firstname)=> {
+     firstname = readline.question("Enter valid First name: ")
+      if (NAME_PATTERN.test(firstname)) {
+         console.log("valid First Name:",firstname);
+      }
+      else {
+          console.log("Invalid First Name");
+        }
+  }
 }
 module.exports = new Utility();
